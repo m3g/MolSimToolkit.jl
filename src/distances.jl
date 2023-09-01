dc25e81894bce2b3cb9243cde9f25cb4e6bb009a
Leandro Martinez <lmartine@unicamp.br>
2023-09-01 12:50:53 -0300

    implementing cm

--- src/distances.jl
+++ src/distances.jl
@@ -6,32 +6,16 @@ Function that calculates the distance between the center of mass of the selectio
 """
 function distances(
     pdb_file::String, 
-    trajectory_file::String, 
-    selection1, 
-    selection2; 
-    first=1, last=nothing, stride=1
+    trajectory::Trajectory, 
+    indexes1::AbstractVector{Int}, 
+    indexes2::AbstractVector{Int} 
 )
-    trajectory = Chemfiles.Trajectory(trajectory_file)
-    atoms1 = PDBTools.readPDB(pdb_file, selection1)
-    atoms2 = PDBTools.readPDB(pdb_file, selection2)
-    last = isnothing(last) ? Chemfiles.length(trajectory) : last
-    nframes = floor(Int,(last - first + 1) / stride)
-    @show nframes, last, first, stride
-    distances = zeros(nframes)
-    # read first frame
-    frame = Chemfiles.read(trajectory)
-    for iframe in 1:min(Chemfiles.length(trajectory), last)
-        if (iframe < first) || ((iframe % stride) != 0)
-            Chemfiles.read!(trajectory, frame)
-            continue
-        end
+    distances = zeros(nframes(trajectory))
+    for frame in trajectory
         unitcell = UnitCell(frame)
-        # read coordinates, convert them to small static vectors
         coordinates = Positions(frame)
-        # TODO: below we compute the distance between the first atom of each selection
-        # this has to be replaced by the computation of their centers of mass
-        x = coordinates[atoms1[1].index]
-        y = coordinates[atoms2[1].index]
+        cm1 = centerofmass(coordinates, indexes1)
+        cm2 = centerofmass(coordinates, indexes2)
         # wrap coordinates according to PBC. Note that here we use an
         # internal function of `CellListMap`. 
         y_wrapped = wrap(y,x,unitcell)
@@ -40,22 +24,19 @@ function distances(
         # add data to distance array
         distances[iframe] = d
         # read next frame from the trajectory file
-        if iframe < last  
-            Chemfiles.read!(trajectory, frame)
-        end
     end
-    Chemfiles.close(trajectory)
     return distances
 end
 
 @testitem "distances" begin
-    using Chemfiles
     using PDBTools
     using MolSimToolkit.Testing
 
-    traj = Testing.namd_traj
+    traj = Trajectory(Testing.namd_traj)
     pdb = Testing.namd_pdb
-    @test distances(pdb, traj, "name CA and residue 1", "name CA and residue 2") ≈ 
+    i1 = selindex(pdb, "name CA and residue 1")
+    i2 = selindex(pdb, "name CA and residue 2")
+    @test distances(pdb, traj, i1, i2) ≈ 
         [3.85915490559465, 3.8968489296128737, 3.8413137354273266, 3.924179031288244, 3.9073096020456592]
 
 end
