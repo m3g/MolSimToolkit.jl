0d8f43895f71201f624a46de40ddc0b42bb2d44d
Leandro Martinez <lmartine@unicamp.br>
2023-09-03 14:14:38 -0300

    set doctest to false

--- docs/make.jl
+++ docs/make.jl
@@ -7,6 +7,7 @@ using MolSimToolkit
 makedocs(
     modules = [MolSimToolkit],
     sitename = "MolSimToolkit.jl",
+    doctest = false, 
     pages = [
         "Home" => "index.md",
         "Functions" => "functions.md",
