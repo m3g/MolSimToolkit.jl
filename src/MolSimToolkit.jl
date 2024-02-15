97baae926d8724b4b86605661e870b7056c2fa81
LV-IQ <l236550@dac.unicamp.br>
2024-02-15 17:51:03 -0300

    "include" rework

--- src/MolSimToolkit.jl
+++ src/MolSimToolkit.jl
@@ -32,7 +32,7 @@ include("./BlockAverages.jl")
 include("./MolecularMinimumDistances/MolecularMinimumDistances.jl")
 @reexport using .MolecularMinimumDistances
 include("./gromacs/remd.jl")
-include("./Resampling/resampling.jl")
+include("./Resampling/reweighting.jl")
 
 # Simulation setup facilities
 include("./PackmolInputCreator/PackmolInputCreator.jl")
