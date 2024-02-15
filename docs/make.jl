dc7d5f425059452f182cda0ffac86f1b4106fde6
LV-IQ <l236550@dac.unicamp.br>
2024-02-15 17:49:53 -0300

    renamed section

--- docs/make.jl
+++ docs/make.jl
@@ -15,7 +15,7 @@ makedocs(
         "Block averages" => "block_averages.md",
         "Replica exchange" => "remd.md",
         "Functions" => "functions.md",
-        "Resampling"  => "Resampling.md",
+        "Reweighting"  => "Reweighting.md",
         "Developer zone" => "Developer.md",
     ],
 )
