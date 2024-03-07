3ec992906fac98407841c89a1b414a62de952d8c
Leandro Martinez <lmartine@unicamp.br>
2024-03-07 09:51:06 -0300

    add probability type option to remd heatmap

--- ext/REMDPlotting.jl
+++ ext/REMDPlotting.jl
@@ -7,13 +7,16 @@ import Plots: plot, plot!, annotate!, heatmap, cgrad, text
     heatmap(data::GromacsREMDlog;
         xlabel="replica",
         ylabel="level",
-        colorbar_title="probability ÷ (1/nreplicas)",
         fontfamily="Computer Modern",
+        probability_type::Symbol=:relative,
         kargs...
     )
 
 Function to plot the probability matrix of a (H)REMD simulation. The probability matrix
 contains the probability of finding each replica at each level of perturbation.
+
+Use `probability_type=:relative` to plot the probability matrix normalized by the number
+of replicas, or `probability_type=:absolute` to plot the probability matrix without normalization.
 
 # Example
 
@@ -26,28 +29,41 @@ julia> heatmap(data)
 ```
 
 """
-function heatmap(data::GromacsREMDlog;
+function heatmap(
+    data::GromacsREMDlog;
     xlabel="replica",
     ylabel="level",
-    colorbar_title="probability ÷ (1/$(size(data.probability_matrix,1)))",
     fontfamily="Computer Modern",
+    probability_type::Symbol=:relative,
+    colorbar_title=nothing,
     kargs...
 )
-    m = data.probability_matrix
-    nreplicas = size(m, 1)
+    nreplicas = size(data.probability_matrix, 1)
+    if probability_type == :relative
+        normalization = 1 / nreplicas
+        digits = 1
+        isnothing(colorbar_title) && (colorbar_title = "probability ÷ (1/$nreplicas)")
+    elseif probability_type == :absolute
+        normalization = 1
+        digits = 2
+        isnothing(colorbar_title) && (colorbar_title = "probability - (ideal = $(round(1/nreplicas; digits=3)))")
+    else
+        throw(ArgumentError("Invalid probability type."))
+    end
+    m = data.probability_matrix ./ normalization
     plt = heatmap(
-        transpose(m ./ (1 / nreplicas));
+        transpose(m);
         xlabel, ylabel, colorbar_title, fontfamily,
-        clims=(0, maximum(m ./ (1 / nreplicas))),
+        clims=(0, maximum(m)),
         color=cgrad(:tempo),
         xticks=(1:nreplicas, 0:nreplicas-1),
         yticks=(1:nreplicas, 0:nreplicas-1),
         framestyle=:box,
     )
     for i in axes(m, 2), j in axes(m, 1)
-        color = m[j, i] > 0.2 ? :white : :black
+        color = m[j, i] > 0.6 * maximum(m) ? :white : :black
         annotate!(plt, j, i,
-            text("$(round(m[j, i] / (1/nreplicas),digits=1))", :center, color, 7, "Computer Modern")
+            text("$(round(m[j, i]; digits))", :center, color, 7, "Computer Modern")
         )
     end
     return plt
