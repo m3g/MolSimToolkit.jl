705e4f3e9415b224101845d746f9a7bcc0e2e4dd
Leandro Martinez <lmartine@unicamp.br>
2024-03-15 11:12:31 -0300

    add and organize test files

--- src/Testing.jl
+++ src/Testing.jl
@@ -3,7 +3,10 @@ module Testing
 src_dir = @__DIR__ # current directory
 test_dir = joinpath(src_dir, "..", "test")
 
-namd_pdb = joinpath(test_dir, "data/namd", "structure.pdb")
-namd_traj = joinpath(test_dir, "data/namd", "structure.dcd")
+namd_pdb = joinpath(test_dir, "data/namd/protein_in_popc_membrane", "structure.pdb")
+namd_traj = joinpath(test_dir, "data/namd/protein_in_popc_membrane", "trajectory.dcd")
+
+namd2_pdb = joinpath(test_dir, "data/namd/protein_in_water_tmao/structure.pdb")
+namd2_traj = joinpath(test_dir, "data/namd/protein_in_water_tmao/trajectory.dcd")
 
 end # module Testing
