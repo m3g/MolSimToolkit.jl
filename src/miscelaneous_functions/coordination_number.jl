1b1a16ee02ab5f8a9e9cc892860f344276e6ab52
Leandro Martinez <lmartine@unicamp.br>
2025-01-31 09:01:27 -0300

    support disabling show progress

--- src/miscelaneous_functions/coordination_number.jl
+++ src/miscelaneous_functions/coordination_number.jl
@@ -4,18 +4,23 @@
         solute::AbstractVector{<:PDBTools.Atom},
         solvent::AbstractVector{<:PDBTools.Atom};
         solvent_natomspermol::Int,
-        cutoff::Real
+        cutoff::Real,
+        show_progress::Bool = true
     )
 
 Calculate the coordination number of the solute atoms with the solvent atoms.
 
-# Arguments
+# Positional Arguments
 
 - `sim::Simulation`: Simulation object.
 - `solute::AbstractVector{<:PDBTools.Atom}`: Vector of solute atoms.
 - `solvent::AbstractVector{<:PDBTools.Atom}`: Vector of solvent atoms.
+
+# Keyword Arguments
+
 - `solvent_natomspermol::Int`: Number of atoms per solvent molecule.
 - `cutoff::Real`: Cutoff distance.
+- `show_progress::Bool`: Show progress bar.
 
 # Returns
 
@@ -32,7 +37,7 @@ julia> protein = select(atoms(sim), "protein");
 
 julia> tmao = select(atoms(sim), "resname TMAO");
 
-julia> coordination_number(sim, protein, tmao; solvent_natomspermol=14, cutoff=3.0)
+julia> coordination_number(sim, protein, tmao; solvent_natomspermol=14, cutoff=3.0, show_progress=false)
 20-element Vector{Int64}:
   7
   3
@@ -48,7 +53,8 @@ function coordination_number(
     solute::AbstractVector{<:PDBTools.Atom},
     solvent::AbstractVector{<:PDBTools.Atom};
     solvent_natomspermol::Int,
-    cutoff::Real
+    cutoff::Real,
+    show_progress::Bool = true,
 )
     if length(solvent) % solvent_natomspermol != 0
         throw(ArgumentError("""\n
@@ -68,13 +74,15 @@ function coordination_number(
         cutoff=cutoff,
         unitcell=unitcell(current_frame(sim)),
     )
-    @showprogress for (iframe, frame) in enumerate(sim)
+    prg = Progress(length(sim); enabled=show_progress)
+    for (iframe, frame) in enumerate(sim)
         p = positions(frame)
         sys.xpositions .= @view(p[inds_solvent])
         sys.ypositions .= @view(p[inds_solute])
         sys.unitcell = unitcell(frame)
         md_list = minimum_distances!(sys)
         cn[iframe] = count(md -> md.within_cutoff, md_list)
+        next!(prg)
     end
     return cn
 end
@@ -84,7 +92,11 @@ end
     sim = Simulation(Testing.namd2_pdb, Testing.namd2_traj)
     protein = select(atoms(sim), "protein")
     tmao = select(atoms(sim), "resname TMAO")
-    cn = coordination_number(sim, protein, tmao; solvent_natomspermol=14, cutoff=3.0)
+    cn = coordination_number(
+        sim, protein, tmao; 
+        solvent_natomspermol=14, cutoff=3.0,
+        show_progress=false
+    )
     @test cn == [7, 3, 4, 5, 6, 5, 7, 7, 10, 5, 4, 4, 5, 6, 2, 9, 4, 5, 2, 6]
 end
 
