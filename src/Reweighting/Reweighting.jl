27e42c3129f3fdc973e1e39126aa25310e429d87
LV-IQ <l236550@dac.unicamp.br>
2025-06-26 14:52:44 -0300

    Add function export

--- src/Reweighting/Reweighting.jl
+++ src/Reweighting/Reweighting.jl
@@ -7,7 +7,7 @@ using ..MolSimToolkit.MolecularMinimumDistances
 import OrderedCollections
 import PDBTools
 
-export reweight, multiple_perturbations_reweight, lennard_jones_perturbation, Perturbation, SystemPerturbations, gauss
+export reweight, multiple_perturbations_reweight, lennard_jones_perturbation, Perturbation, SystemPerturbations, gauss, conv_to_df
 
 const testdir = "$(@__DIR__)/test"
 
