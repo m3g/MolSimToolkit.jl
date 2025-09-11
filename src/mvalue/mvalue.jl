2af63690cd1b5a05dec8bcf9ae75894773bc2db0
Leandro Martinez <lmartine@unicamp.br>
2025-09-11 09:31:09 -0300

    support other cossolvents / code adjustment

--- src/mvalue/mvalue.jl
+++ src/mvalue/mvalue.jl
@@ -1,6 +1,7 @@
 using PDBTools
 
 export mvalue, parse_mvalue_server_sasa, run_gmx_sasa
+#export sasa_desnat_average
 
 abstract type CosolventType end
 struct Urea <: CosolventType end
@@ -83,63 +84,27 @@ The backbone contribution is assumed to be the same for all amino acids and is c
 using the backbone ASA of glycine, according to the universal model of Moeser and Horinek.
 
 =#
-function tfe_asa(::Type{Urea}, restype::AbstractString)
-    isolated_ASA = Dict{String,Tuple{Float64,Float64}}( 
-                   # BB      SC   (Å^2)
-        "ALA"	=> (46.2,	71.9),
-        "PHE"	=> (38.4,	184.4),
-        "LEU"	=> (35.3,	157.8),
-        "ILE"	=> (30.9,	150.1),
-        "VAL"	=> (36.1,	128.4),
-        "PRO"	=> (35.6,	111.0),
-        "MET"	=> (38.6,	164.8),
-        "TRP"	=> (37.4,	228.9),
-        "GLY"	=> (88.1,	0),
-        "SER"	=> (44.0,	85.8),
-        "THR"	=> (37.9,	114.6),
-        "TYR"	=> (38.7,	198.1),
-        "GLN"	=> (37.8,	155.4),
-        "ASN"	=> (40.2,	125.3),
-        "ASP"	=> (40.5,	118.2),
-        "GLU"	=> (37.8,	148.4),
-        "HIS"	=> (40.4,	162.1),
-        "LYS"	=> (38.7,	187.1),
-        "ARG"	=> (39.1,	216.9),
-        "CYS"	=> (42.6,	103.5),
-    ) 
-
-    dg_aa = Dict( # cal/mol (1M)
-        "BB" => -39,
-        "ALA" => 1.01,
-        "PHE" => -68.64,
-        "LEU" => -40.10,
-        "ILE" => -23.96,
-        "VAL" => -7.18,
-        "PRO" => -3.18,
-        "MET" => -33.87,
-        "TRP" => -126.99,
-        "GLY" => 0.00,
-        "SER" => -6.09,
-        "THR" => -7.62,
-        "TYR" => -30.61,
-        "GLN" => -40.34,
-        "ASN" => -24.32,
-        "ASP" => 18.02,
-        "GLU" => 15.09,
-        "HIS" => -36.04,
-        "HSD" => -36.04,
-        "HSE" => -36.04,
-        "LYS" => -8.29,
-        "ARG" => -6.70,
-        "CYS" => 0.00,
+function tfe_asa(::Type{Urea}, restype::AbstractString;
+    tfe_sc_bb = tfe_sc_bb_moeser_and_horinek,
+    isolated_ASA = isolated_ASA,
+)
+    col = 7 # for urea in data tables 
+    return _tfe_sasa(
+        restype,
+        tfe_sc_bb["BB"][col],
+        tfe_sc_bb[restype][col],
+        isolated_ASA["GLY"],
+        isolated_ASA[restype],
     )
+end
 
+function _tfe_sasa(restype, tfe_bb, tfe_sc_restype, isolated_ASA_GLY, isolated_ASA_restype)
     # converted to kcal/nm^2 here
-    bb_contribution = (dg_aa["BB"]/1000) / (first(isolated_ASA["GLY"])/100) # universal model: equal for all AAs
+    bb_contribution = (tfe_bb/1000) / (first(isolated_ASA_GLY)/100) # universal model: equal for all AAs
     if restype == "GLY"
         return 0.0, bb_contribution
     end
-    return (dg_aa[restype]/1000) / (last(isolated_ASA[restype])/100), bb_contribution # side chain
+    return (tfe_sc_restype/1000) / (last(isolated_ASA_restype)/100), bb_contribution # side chain, backbone
 end
 
 #=
@@ -285,3 +250,136 @@ function run_gmx_sasa_single(pdbname, resname)
     rm(index_file, force=true)
     return sasa_bb, sasa_sc
 end
+
+#
+# Data section
+#
+
+#=
+
+Amino acid side-chain and peptide backbone unit transfer free energies (cal/mol) from water to 1M osmolyte
+Supplementary Table 1 of https://doi.org/10.1073/pnas.0507053102
+
+=#
+const tfe_sc_bb_moeser_and_horinek = Dict(
+#                TMAO   Sarcosine     Betaine     Proline    Sorbitol    Sucrose         Urea
+    "ALA" => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,       1.01),
+    "PHE" => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,     -68.64),
+    "LEU" => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,     -40.10),
+    "ILE" => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,     -23.96),
+    "VAL" => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,      -7.18),
+    "PRO" => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,      -3.18),
+    "MET" => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,     -33.87),
+    "TRP" => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,    -126.90),
+    "GLY" => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,       0.00),
+    "SER" => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,      -6.09),
+    "THR" => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,      -7.62),
+    "TYR" => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,     -30.61),
+    "GLN" => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,     -40.34),
+    "ASN" => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,     -24.32),
+    "ASP" => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,      18.02),
+    "GLU" => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,      15.09),
+    "HIS" => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,     -36.04),
+    "HSD" => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,     -36.04),
+    "HSE" => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,     -36.04),
+    "LYS" => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,      -8.29),
+    "ARG" => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,      -6.70),
+    "CYS" => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,       0.00),
+    "BB"  => (   0.00,       0.00,       0.00,       0.00,       0.00,       0.00,        -39),
+)
+
+#=
+
+Amino acid side-chain and peptide backbone unit transfer free energies (cal/mol) from water to 1M osmolyte
+Supplementary Table 1 of https://doi.org/10.1073/pnas.0507053102
+
+=#
+const tfe_sc_bb_auton_and_bolen = Dict(
+#                TMAO   Sarcosine     Betaine     Proline    Sorbitol    Sucrose         Urea
+    "ALA" => ( -14.64,      10.91,       4.77,      -0.07,      16.57,      22.05,      -4.69),
+    "PHE" => (  -9.32,     -12.64,    -112.93,     -71.26,      26.38,     -96.35,     -83.11),
+    "LEU" => (  11.62,      38.33,     -17.73,       4.77,      39.07,      37.11,     -54.57),
+    "ILE" => ( -25.43,      39.98,      -1.27,      -2.72,      36.90,      28.12,     -38.43),
+    "VAL" => (  -1.02,      29.32,     -19.63,       7.96,      24.65,      33.92,     -21.65),
+    "PRO" => (-137.73,     -34.23,    -125.16,     -63.96,      -4.48,     -73.02,     -17.65),
+    "MET" => (  -7.65,       8.18,     -14.16,     -35.12,      20.97,      -6.66,     -48.34),
+    "TRP" => (-152.87,    -113.03,    -369.93,    -198.37,     -67.23,    -215.27,    -141.46),
+    "GLY" => (      0,          0,          0,          0,          0,          0,          0),
+    "SER" => ( -39.04,     -27.98,     -41.85,     -33.49,      -1.58,      -2.79,     -20.56),
+    "THR" => (   3.57,      -7.54,       0.33,     -18.33,      13.20,      20.82,     -22.09),
+    "TYR" => (-114.32,     -26.37,    -213.09,    -138.41,     -53.50,     -78.41,     -45.08),
+    "GLN" => (  41.41,     -10.19,       7.57,     -32.26,     -23.98,     -40.87,     -54.81),
+    "ASN" => (  55.69,     -40.93,      33.17,     -17.71,     -21.21,     -28.28,     -38.79),
+    "ASP" => ( -66.67,     -14.20,    -116.56,     -90.51,     -83.88,     -37.17,       3.55),
+    "GLU" => ( -83.25,     -12.61,    -112.08,     -89.17,     -70.05,     -41.65,       0.62),
+    "HIS" => (  42.07,     -20.80,     -35.97,     -45.10,     -42.45,    -118.66,     -50.51),
+    "LYS" => (-110.23,     -27.42,    -171.99,     -59.87,     -32.47,     -39.60,     -22.76),
+    "ARG" => (-109.27,     -32.24,    -109.45,     -60.18,     -24.65,     -79.32,     -21.17),
+    "CYS" => (      0,          0,          0,          0,          0,          0,          0), # not reported
+    "BB"  => (     90,         52,         67,         48,         35,         62,        -39),
+)
+
+
+#= 
+
+Isolated ASA values are from the Supporting Table 2 of https://doi.org/10.1073/pnas.0507053102
+(https://www.pnas.org/doi/suppl/10.1073/pnas.0507053102/suppl_file/07053table2.pdf)
+
+=#
+const isolated_ASA = Dict{String,Tuple{Float64,Float64}}( 
+                # BB      SC   (Å^2)
+    "ALA"	=> (46.2,	71.9),
+    "PHE"	=> (38.4,	184.4),
+    "LEU"	=> (35.3,	157.8),
+    "ILE"	=> (30.9,	150.1),
+    "VAL"	=> (36.1,	128.4),
+    "PRO"	=> (35.6,	111.0),
+    "MET"	=> (38.6,	164.8),
+    "TRP"	=> (37.4,	228.9),
+    "GLY"	=> (88.1,	0),
+    "SER"	=> (44.0,	85.8),
+    "THR"	=> (37.9,	114.6),
+    "TYR"	=> (38.7,	198.1),
+    "GLN"	=> (37.8,	155.4),
+    "ASN"	=> (40.2,	125.3),
+    "ASP"	=> (40.5,	118.2),
+    "GLU"	=> (37.8,	148.4),
+    "HIS"	=> (40.4,	162.1),
+    "LYS"	=> (38.7,	187.1),
+    "ARG"	=> (39.1,	216.9),
+    "CYS"	=> (42.6,	103.5),
+) 
+
+#=
+
+Average SASA values from lower and upper bounds of the denatured state ensemble,
+reported in Supplementary Table 2 of https://doi.org/10.1073/pnas.0507053102
+
+These are the average values of Table 1 of https://doi.org/10.1021/bi962819o
+
+Can be used for testing, but **do not** provide the same accuracy as the values
+calculated with GROMACS or obtained from the m-value server.
+
+=# 
+const sasa_desnat_average = Dict(
+    "ALA" => Dict(:bb => 27.9,	:sc => 55.1),
+    "PHE" => Dict(:bb => 24.3,	:sc => 128.8),
+    "LEU" => Dict(:bb => 22.7,	:sc => 109.6),
+    "ILE" => Dict(:bb => 20.0,	:sc => 117.1),
+    "VAL" => Dict(:bb => 20.4,	:sc => 96.4),
+    "PRO" => Dict(:bb => 22.5,	:sc => 87.0),
+    "MET" => Dict(:bb => 25.3,	:sc => 122.4),
+    "TRP" => Dict(:bb => 23.6,	:sc => 156.6),
+    "GLY" => Dict(:bb => 65.2,	:sc => 0.0),
+    "SER" => Dict(:bb => 29.4,	:sc => 66.5),
+    "THR" => Dict(:bb => 24.1,	:sc => 84.3),
+    "TYR" => Dict(:bb => 25.6,	:sc => 141.7),
+    "GLN" => Dict(:bb => 25.3,	:sc => 116.9),
+    "ASN" => Dict(:bb => 25.2,	:sc => 90.1),
+    "ASP" => Dict(:bb => 26.0,	:sc => 87.0),
+    "GLU" => Dict(:bb => 25.7,	:sc => 113.4),
+    "HIS" => Dict(:bb => 24.2,	:sc => 111.5),
+    "LYS" => Dict(:bb => 26.1,	:sc => 150.7),
+    "ARG" => Dict(:bb => 25.1,	:sc => 171.1),
+    "CYS" => Dict(:bb => 26.4,	:sc => 73.0),
+)
