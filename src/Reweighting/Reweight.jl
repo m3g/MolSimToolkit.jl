cc2a4c6c244d2654390f568f142c4ac431d4aefc
LV-IQ <l236550@dac.unicamp.br>
2025-11-27 16:39:37 -0300

    Minor fix

--- src/Reweighting/Reweight.jl
+++ src/Reweighting/Reweight.jl
@@ -232,7 +232,7 @@ function reweight(
                         pert_input.perturbations[pk].subgroup1, 
                         pert_input.perturbations[pk].subgroup2, 
                         pert_input.perturbations[pk].perturbation_function,
-                        res_dic[i][4],
+                        res_dic[pk][4],
                         iframe),
                         system
                 )
